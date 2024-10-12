b2beb9bbd89565d11ca7214659305f149e3c29ba
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-10-12 11:19:27 -0300

    js

--- begginer javascript/sum.js
+++ begginer javascript/sum.js
@@ -1,7 +1,12 @@
-function som(){
+/*function som(){
     var num1 = document.getElementById("text1");
     var num2 = document.getElementById("text2");
     var result = eval(num1.value) + eval(num2.value);
     let msg= document.getElementById("text3")
-    msg.innerHTML = result;}
-    
+    msg.innerHTML = result;}*/
+function som() {
+    var num1 = document.getElementById("text1").value;
+    var num2 = document.getElementById("text2").value;
+    var result = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("result").value = result;
+}    
