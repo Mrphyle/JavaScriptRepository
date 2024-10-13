40ce6977abc740a229f66d4367cacba3baca9498
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-10-13 12:36:10 -0300

    Update media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+número1= input("Digite o primeiro número: ")
+número2= input("Digite o segundo número: ")
+número3= input("Digite o terceiro número: ")
+número4= input("Digite o quarto número: ")
+resultado= (número1 + número2 + número3 + número4)/4
+print(resultado)
