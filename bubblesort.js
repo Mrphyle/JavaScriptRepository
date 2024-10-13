f275e214ffefac0b46596b753e16d68bea77252f
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-10-13 18:21:27 -0300

    up bubblesort.js and add bubblesort.py

--- bubblesort.js
+++ bubblesort.js
@@ -1,25 +1,31 @@
 //npm install prompt-sync
-const prompt = require('prompt-sync')();
-let list = []; //90,10,70,5,4,6,100,11,0,1,8
-var inputnumberslist = parseInt(prompt("when numbers you want add in this list? "))
-for (let i = 0; i < inputnumberslist; i++){
-    let inputnumberINlist = parseInt(prompt("add the number:  "))
-    list.push(inputnumberINlist)
-}
-function bubbleSort(){
-    let num = list.length;
-    let ligado;
- do { ligado = false;
-    for (let i = 0; i < num -1; i++){
-        if (list [i] > list [ i + 1]){
-            let temp = list[i] ;
-            list[i] = list[i+1];
-            list[i+1] = temp;
-            ligado = true;
-        } 
+do{
+    const prompt = require('prompt-sync')();
+    let list = []; //90,10,70,5,4,6,100,11,0,1,8
+    var inputnumberslist = parseInt(prompt("when numbers you want add in this list? "))
+    for (let i = 0; i < inputnumberslist; i++){
+        let inputnumberINlist = parseInt(prompt("add the number:  "))
+        list.push(inputnumberINlist)
     }
-    num--;
-}while(ligado);
-}
-bubbleSort()
-console.log(list)
+    function bubbleSort(){
+        let num = list.length;
+        let ligado;
+     do { ligado = false;
+        for (let i = 0; i < num -1; i++){
+            if (list [i] > list [ i + 1]){
+                let temp = list[i] ;
+                list[i] = list[i+1];
+                list[i+1] = temp;
+                ligado = true;
+            } 
+        }
+        num--;
+    }while(ligado);
+    }
+    bubbleSort()
+    console.log(list)
+    var restartcode= prompt("you want restart this code? ")
+    if (restartcode.toLowerCase() != "y"){
+        break;
+    }
+}while(true);
