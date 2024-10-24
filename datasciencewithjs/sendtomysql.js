567d94111e53e7d6bee2cf46900540eecf04facd
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-10-24 11:40:54 -0300

    teste +"10" in 10 type int

---
+++ datasciencewithjs/sendtomysql.js
@@ -0,0 +1,13 @@
+/*let  most_user = "40"
+console.log(typeof most_user);
+var intstring= +most_user
+console.log(typeof intstring);*/
+const prompt= require('prompt-sync')();
+let number1 = +prompt("Enter the first number: ");
+console.log(typeof number1,"\n")
+let number2 = +prompt("Enter the second number: ");
+console.log(typeofnumber2,"\n")
+let number3 = +prompt("Enter the three number: ");
+console.log(typeof number3,"\n")
+avarege =  (number1 + number2 + number3) / 3;
+console.log(avarege);
